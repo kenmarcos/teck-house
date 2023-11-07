399730b909b758b04759501502606d442a283cf5
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2023-11-07 14:29:33 -0300

    feat: empty the cart after payment confirmation

--- src/app/(pages)/orders/page.tsx
+++ src/app/(pages)/orders/page.tsx
@@ -1,10 +1,15 @@
 "use client";
 
+import { useCartStore } from "@/store/cart";
 import React, { useEffect } from "react";
 
 const OrdersPage = () => {
   useEffect(() => {
     localStorage.removeItem("@teck-house:cart");
+
+    useCartStore.setState({
+      products: [],
+    });
   }, []);
 
   return <div>OrdersPage</div>;
